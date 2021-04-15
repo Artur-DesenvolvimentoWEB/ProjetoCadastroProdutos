00965d87f1830a71a3df8e0767f8055035c33f1c
Artur Campos <arturcamposba@gmail.com>
2021-04-15 14:10:48 -0300

    Atividade2

--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -1,13 +1,17 @@
-//const User = require("../models/User");
+//const Product = require("../models/Product");
 import * as Yup from 'yup';
 import Produtos from "../models/Produtos.js";
 
 class ProdutosController {
-    // GET /Produtos > Listar Produtos
+    // GET /products > Listar produtos
     // errors code: 100..109
+    // ?page=1&limit=10
     async list(req, res) {
-        // consultar no banco os Produtos
-        Produtos.find({}).then((produtos) => {
+        // consultar no banco os produtos
+        const limit = req.query.limit || 3;
+        const page = req.query.page || 1;
+
+        Produtos.paginate({}, {page, limit}).then((produtos) => {
             return res.json({
                 error: false,
                 produtos: produtos
@@ -20,13 +24,13 @@ class ProdutosController {
             });
         });
         }
-    // GET /users/:id > Listar um usuário
+    // GET /products/:id > Listar um produto
     // errors code: 110..119
     async listOne(req, res) {
-        Produtos.findOne({ _id: req.params.id }, '_id nome marca categoria createAt updateAt').then((produtos) => {
+        Produtos.findOne({ _id: req.params.id }, '_id nome marca categoria createAt updateAt').then((Produtos) => {
             return res.json({
                 error: false,
-                produtos: produtos
+                Produtos: Produtos
             });
         }).catch((err) => {
             return res.status(400).json({
@@ -36,7 +40,7 @@ class ProdutosController {
             })
         })
     }
-    // POST /users
+    // POST /products
     // errors code: 120..129
     async create(req, res) {
         // Validação com Yup
@@ -44,7 +48,7 @@ class ProdutosController {
             nome: Yup.string()
                 .required(),
             marca: Yup.string()
-                .required(),
+            .required(),
             categoria: Yup.string()
                 .required()
         });
@@ -56,36 +60,27 @@ class ProdutosController {
                 code: 120,
                 message: err.message
             });
-        };
-        const ProdutoExiste = await Produtos.findOne({ _id: req.body.id });
-        if (ProdutoExiste) {
-            console.log(ProdutoExiste);
-            return res.status(400).json({
-                error: true,
-                code: 121,
-                message: "Error: Este produto já está cadastrado!"
-            });
-        };
-        
+        }
         
         Produtos.create(req.body).then((produtos) => {
-            return res.json(produtos);
+            return res.json({
+                error: false,
+                message: "Produto cadastrado com sucesso!",
+                produtos
+            });
         }).catch((err) => {
             return res.status(400).json({
                 error: true,
                 code: 122,
-                message: "Error: Produto não foi cadastrado com sucesso"
+                message: "Error: produto não foi cadastrado com sucesso"
             });
         });
     }
-    // PUT /users/:id
+    // PUT /products/:id
     // errors code: 130..139
     async update(req, res) {
         // Validação com Yup
         const schema = Yup.object().shape({
-            _id: Yup.string()
-                  .required()
-                  .notOneOf(['']),
             nome: Yup.string()
                 .notOneOf(['']),
             marca: Yup.string()
@@ -103,32 +98,62 @@ class ProdutosController {
             });
         }
         
-        const ProdutoExiste = await Produtos.findOne({_id: req.params.id});
-
-        if(!ProdutoExiste){
+        try {
+            //caso a string não obtiver o comprimento adequado para o ID (catch)
+            const produtoExiste = await Produtos.findOne({_id: req.params.id});
+            if(!produtoExiste){
+                return res.status(400).json({
+                    error: true,
+                    code: 132,
+                    message: "Erro: produto não encontrado!"
+                });
+            };
+        } catch(err) {
             return res.status(400).json({
                 error: true,
                 code: 131,
-                message: "Erro: Produto não encontrado!"
+                message: "Erro: produto não encontrado!"
             });
-        };
+        }
+        
+
+        
 
         Produtos.updateOne({_id: req.params.id}, req.body).then(() => {
             return res.json({
                 error: false,
-                message: "Produto editado com sucesso!"
+                message: "produto editado com sucesso!"
             });
         }).catch((err) => {
             return res.status(400).json({
                 error: true,
                 code: 133,
-                message: "Erro: Produto não foi editado com sucesso!"
+                message: "Erro: produto não foi editado com sucesso!"
             });
         });
     }
-    // DELETE /users/:id
+    // DELETE /products/:id
     // errors code: 140..149
     async delete(req, res) {
+
+        try {
+            //caso a string não obtiver o comprimento adequado para o ID (catch)
+            const produtoExiste = await Produtos.findOne({_id: req.params.id});
+            if(!produtoExiste){
+                return res.status(400).json({
+                    error: true,
+                    code: 140,
+                    message: "Erro: produto não encontrado!"
+                });
+            };
+        } catch(err) {
+            return res.status(400).json({
+                error: true,
+                code: 141,
+                message: "Erro: produto não encontrado!"
+            });
+        }
+
         Produtos.deleteOne({ _id: req.params.id }).then(() => {
             return res.json({
                 error: false,
@@ -137,12 +162,12 @@ class ProdutosController {
         }).catch((err) => {
             return res.status(400).json({
                 error: true,
-                code: 140,
-                message: "Error: Produto não foi apagado com sucesso."
+                code: 142,
+                message: "Error: produto não foi apagado com sucesso!"
             });
         });
     }
 }
 
-//module.exports = new UserController();
+//module.exports = new ProdutosController();
 export default new ProdutosController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
-//const { Router } = require("express");
+
 import { Router } from "express";
+
 import ProdutosController from "./controllers/ProdutosController.js";
+import authMiddleware from "./middlewares/auth.js";
+import UserController from "./controllers/UserController.js";
+import LoginController from "./controllers/LoginController.js";
+
 const routes = new Router();
 
-// rotas
 routes.get("/", async (req, res) => {
   res.send("Olá mundo!");
 });
@@ -18,6 +22,21 @@ routes.post("/Produtos", ProdutosController.create);
 routes.put("/Produtos/:id", ProdutosController.update);
 // DELETE /Produtos/:id > Deletar um produto
 routes.delete("/Produtos/:id", ProdutosController.delete); 
+
+/* Login */
+/** Criar a rota de login com o método post. */
+
+/* User */
+routes.post("/users", UserController.create);
+
+/**
+ * Se a autenticação estiver funcionando adequadamente, pode utilizar essas rotas como teste.
+ */
+
+/* Me (usuário autenticado) */
+routes.get("/me", authMiddleware, UserController.listMe);
+routes.put("/me", authMiddleware, UserController.updateMe);
+routes.delete("/me", authMiddleware, UserController.deleteMe);
 
 //module.exports = routes;
 export default routes;
